cdabc6a738972b87de2a70a22f268258028a4f68
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-04-05 19:23:04 +0530

    TC-9-write configserver

---
+++ src/main/java/twitter/clone/configserver/ConfigserverApplication.java
@@ -0,0 +1,16 @@
+package twitter.clone.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/twitter/clone/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package twitter.clone.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
